9f72327e87ec418adb18e484511ce42f223162c7
Yuzu20220719 <shirley-7@outlook.com>
2022-08-12 12:32:47 +0800

    git02-01 v1.0

---
+++ git02-01/src/test/java/com/example/git0201/Git0201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git0201;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git0201ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
